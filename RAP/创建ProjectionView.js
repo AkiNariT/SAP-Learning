e862107afa42066145ffbc44860dccf9346077fd
AkiNariT <103888620+AkiNariT@users.noreply.github.com>
2026-06-16 11:39:57 +0900

    Create 创建ProjectionView.js

---
+++ RAP/创建ProjectionView.js
@@ -0,0 +1,25 @@
+@AccessControl.authorizationCheck: #NOT_REQUIRED
+@EndUserText.label: 'RAP Consumable Request Projection View'
+@Metadata.ignorePropagatedAnnotations: true
+define root view entity ZC_RAP_CONS_REQ 
+    provider contract transactional_query
+    as projection on ZI_RAP_CONS_REQ
+{
+  key RequestID,
+
+      RequestDate,
+      Requester,
+      ItemText,
+
+      Quantity,
+      UnitCode,
+
+      CostCenter,
+      Status,
+
+      CreatedBy,
+      CreatedAt,
+      LastChangedBy,
+      LastChangedAt,
+      LocalLastChangedAt
+}
